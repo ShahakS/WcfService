da6425425f4d2fe7970efee9889832476daf074a
Shahak Shaked <shahak.shaked@gmail.com>
2019-10-20 22:06:17 +0300

    Add project files.

---
+++ WcfServiceClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceClient;
+using WcfServiceClient.ServiceReference;
+
+namespace WcfServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorClient wcf = new CalculatorClient();
+            double res = wcf.Subtract(6, 7);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WcfServiceLib/IService1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLib
+{
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        [return: MessageParameter(Name = "Result")]
+        double Subtract([MessageParameter(Name = "Num1")]double n1, [MessageParameter(Name = "Num2")] double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+}
